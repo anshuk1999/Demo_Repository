bbd98e7c9ca6e68cca82004160ef6027dd963842
anshuk1999 <anshuk.dev21@gmail.com>
2025-08-25 11:33:44 +0530

    Corrected regex

--- DEMO.Data/Entities/Registration.cs
+++ DEMO.Data/Entities/Registration.cs
@@ -1,40 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace DEMO.Data.Entities;
 
 public partial class Registration
 {
     public int Id { get; set; }
-    [Required]
+
     public int CategoryId { get; set; }
 
     public int SubCategoryId { get; set; }
-    [Required(ErrorMessage = "Employee name is required")]
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Only letters are allowed in Name")]
+
     public string EmployeeName { get; set; } = null!;
-    [Range(18, 100, ErrorMessage = "Age must be between 18 and 100")]
+
     public int EmployeeAge { get; set; }
 
-    [Required(ErrorMessage = "Email is required")]
-    [RegularExpression(@"^[^@\s]+@[^@\s]+\.com$", ErrorMessage = "Email must be valid and end with .com")]
     public string EmployeeEmail { get; set; } = null!;
 
-    [Required(ErrorMessage = "Mobile number is required")]
-    [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits")]
     public string EmployeeMobile { get; set; } = null!;
-    [Required]
+
     public string Address { get; set; } = null!;
-    [Required]
+
     public string City { get; set; } = null!;
-    [Required]
+
     public string District { get; set; } = null!;
-    [Required]
+
     public string State { get; set; } = null!;
-    [Required]
+
     public string Country { get; set; } = null!;
-    [Range(0, 1000000, ErrorMessage = "Salary must be between 0 and 1,000,000")]
+
     public decimal EmployeeSalary { get; set; }
 
     public virtual CategoryName Category { get; set; } = null!;
--- DEMO.Dto/RegistrationDto.cs
+++ DEMO.Dto/RegistrationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +10,34 @@ namespace DEMO.Dto
     public class RegistrationDto
     {
         public int Id { get; set; }
-
-        // Dropdown selections
+        [Required]
         public int CategoryId { get; set; }
-        public int SubCategoryId { get; set; }
 
-        // Employee info
-        public string? EmployeeName { get; set; }
+        public int SubCategoryId { get; set; }
+        [Required(ErrorMessage = "Employee name is required")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Only letters are allowed in Name")]
+        public string EmployeeName { get; set; } = null!;
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100")]
         public int EmployeeAge { get; set; }
-        public string? EmployeeEmail { get; set; }
-        public string? EmployeeMobile { get; set; }
-        public string? Address { get; set; }
-        public string? City { get; set; }
-        public string? District { get; set; }
-        public string? State { get; set; }
-        public string? Country { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.com$", ErrorMessage = "Email must be valid and end with .com")]
+        public string EmployeeEmail { get; set; } = null!;
+
+        [Required(ErrorMessage = "Mobile number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits")]
+        public string EmployeeMobile { get; set; } = null!;
+        [Required]
+        public string Address { get; set; } = null!;
+        [Required]
+        public string City { get; set; } = null!;
+        [Required]
+        public string District { get; set; } = null!;
+        [Required]
+        public string State { get; set; } = null!;
+        [Required]
+        public string Country { get; set; } = null!;
+        [Range(0, 1000000, ErrorMessage = "Salary must be between 0 and 1,000,000")]
         public decimal EmployeeSalary { get; set; }
 
         //// Dropdown lists
